7e4b058bdc22d54d69a02af7f684d0f396181a05
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-05-06 08:38:52 +0200

    feat: stopWatch-function added to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,8 +24,15 @@
           <option :value="idForJs">Basic JS</option>
         </select>
       </div>
-      <button type="button" class="startButton">Start the quiz</button>
+      <button type="button" class="startButton" @click="startTheGame()">
+        Start the quiz
+      </button>
     </form>
+    <p class="time-display">
+      <span class="stopWatch">Stopwatch</span>
+      <span class="theMinutes">{{ stopWatchMinutes }}</span> :
+      <span class="theSeconds">{{ stopWatchSeconds }}</span>
+    </p>
   </div>
 </template>
 
@@ -39,12 +46,43 @@ export default {
       theSelectedGroup: "",
       idForHtmlAndCss: "c76668d0-ce3a-48a7-acd5-0f54ad6818e1",
       idForJs: "9d5ae045-ef9a-4068-bc6c-1b102bda5f55",
+      stopWatchMinutes: "00",
+      stopWatchSeconds: "00",
+      stopWatchDoes: "start",
     };
   },
   methods: {
     getDataFromLocalStorage() {
       const allData = JSON.parse(localStorage.getItem("allData"));
       this.backendData = allData;
+    },
+
+    startStopWatch() {
+      const intervallID = setInterval(() => {
+        if (this.stopWatchDoes === "start") {
+          let currentSeconds = Number(this.stopWatchSeconds);
+          currentSeconds++;
+          if (currentSeconds === 60) {
+            let currentMinutes = (Number(this.stopWatchMinutes) + 1).toString();
+            if (currentMinutes.length === 1) {
+              currentMinutes = "0" + currentMinutes;
+            }
+            this.stopWatchMinutes = currentMinutes;
+            this.stopWatchSeconds = "00";
+          } else {
+            currentSeconds = currentSeconds.toString();
+            if (currentSeconds.length === 1) {
+              currentSeconds = "0" + currentSeconds;
+            }
+            this.stopWatchSeconds = currentSeconds;
+          }
+        } else {
+          clearInterval(intervallID);
+        }
+      }, 1000);
+    },
+    startTheGame() {
+      this.startStopWatch();
     },
   },
   created() {
@@ -57,6 +95,7 @@ export default {
 .whole-area {
   display: flex;
   flex-direction: column;
+  position: relative;
 }
 
 h1 {
@@ -84,5 +123,18 @@ label {
 
 .startButton {
   width: 10rem;
+}
+
+.time-display {
+  font-size: 3rem;
+  border: 0.5rem solid green;
+  max-width: fit-content;
+  padding: 1rem;
+}
+
+.stopWatch {
+  display: block;
+  font-size: 1rem;
+  text-decoration: underline;
 }
 </style>
